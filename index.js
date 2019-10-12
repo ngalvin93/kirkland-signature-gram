151b6783e84bdf74417653fefba02ad5c2f94497
Alvin Ng <ngalvin93@gmail.com>
2019-10-12 12:55:06 -0500

    more modules

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
+// --------------------------------------------------------------------------------------
+// NODE MODULES
+
+const passport = require('passport')
 const express = require('express')
 const app = express()
+const fs = require('fs')
+
 const port = process.env.PORT || 3000
 
-
+// --------------------------------------------------------------------------------------
+// EXPRESS ENDPOINTS
 
 app.get('/', function (req, res) {
     res.send('Hello')
 })
 
+// --------------------------------------------------------------------------------------
+// EXPRESS PORT
 
 app.listen(port, function () {
     console.log('Listening on port ' + port + '👍')
