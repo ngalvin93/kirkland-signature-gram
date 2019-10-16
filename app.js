9ac01b48156294268c1134750e26cb6dda4664d4
Alvin Ng <ngalvin93@gmail.com>
2019-10-15 23:35:33 -0500

    delete users.js

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 
 var indexRouter = require('./routes/index')
-var usersRouter = require('./routes/users')
 var accountRouter = require('./routes/accounts') // automatically looks for index.js in the specified directory
 
 var app = express()
@@ -23,7 +22,6 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
 app.use('/account', accountRouter)
 
 // catch 404 and forward to error handler
