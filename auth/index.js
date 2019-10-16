81100aa443e73760c6c48c5f75b103708bd2e216
Alvin Ng <ngalvin93@gmail.com>
2019-10-15 23:12:06 -0500

    auth routes

--- auth/index.js
+++ auth/index.js
@@ -2,15 +2,27 @@ const express = require('express')
 const router = express.Router()
 
 // Routes below are prepended with /auth from mounting on app.js
-router.get('/', function (req, res) {
+router.get('/login', function (req, res) {
   res.json({
-    message: '👍🏻'
+    message: 'Login page with form'
   })
 })
 
-router.post('/signup', function (req, res) {
+router.post('/login', function (req, res) {
   res.json({
-    message: '✨'
+    message: 'Attemping to send login information'
+  })
+})
+
+router.get('/register', function (req, res) {
+  res.json({
+    message: 'Register page with form'
+  })
+})
+
+router.post('/register', function (req, res) {
+  res.json({
+    message: 'Attempting to send register information'
   })
 })
 
