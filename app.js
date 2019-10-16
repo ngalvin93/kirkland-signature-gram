099faa6c14da9662608e75df3387bc3260566cb8
Alvin Ng <ngalvin93@gmail.com>
2019-10-15 23:20:34 -0500

    auth to account

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan')
 
 var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users')
-var authRouter = require('./auth') // automatically looks for index.js in the specified directory
+var accountRouter = require('./account') // automatically looks for index.js in the specified directory
 
 var app = express()
 
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
-app.use('/auth', authRouter)
+app.use('/account', accountRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
