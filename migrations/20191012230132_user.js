915b46fcb8af66fb99a86a1e71ce8bfb10241dae
Alvin Ng <ngalvin93@gmail.com>
2019-10-19 10:32:50 -0500

    update schema and seed

--- migrations/20191012230132_user.js
+++ migrations/20191012230132_user.js
@@ -2,9 +2,7 @@
 exports.up = function (knex) {
   return knex.schema.createTable('User', function (table) {
     table.increments('userId')
-    table.string('firstName', 25)
-      .notNullable()
-    table.string('lastName', 25)
+    table.string('fullName', 25)
       .notNullable()
     table.string('username', 25)
       .notNullable()
--- seeds/01_User.js
+++ seeds/01_User.js
@@ -4,11 +4,11 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('User').insert([
-        { firstName: 'Kirkland', lastName: 'Signature', username: 'kirksig', email: 'kirkland@gmail.com', password: '123456', bio: 'The original.' },
-        { firstName: 'Mariah', lastName: 'Carey', username: 'mcarey', email: 'mcarey@music.com', password: 'abcdef', bio: 'I sing falseto notes very good.' },
-        { firstName: 'Frank', lastName: 'Ocean', username: 'franko', email: 'endless@music.com', password: '654321', bio: 'I am a singer/song writer.' },
-        { firstName: 'Selena', lastName: 'Gomez', username: 'sgomez', email: 'selenag@gmail.com', password: 'fedcba', bio: 'Disney kid for life!!' },
-        { firstName: 'James', lastName: 'Harden', username: 'jharden13', email: 'jharden13@rockets.com', password: '000000', bio: 'Rockets champions 2020 #NBA' }
+        { fullName: 'Kirkland Signature', username: 'kirksig', email: 'kirkland@gmail.com', password: '123456', bio: 'The original.' },
+        { fullName: 'Mariah Carey', username: 'mcarey', email: 'mcarey@music.com', password: 'abcdef', bio: 'I sing falseto notes very good.' },
+        { fullName: 'Frank Ocean', username: 'franko', email: 'endless@music.com', password: '654321', bio: 'I am a singer/song writer.' },
+        { fullName: 'Selena Gomez', username: 'sgomez', email: 'selenag@gmail.com', password: 'fedcba', bio: 'Disney kid for life!!' },
+        { fullName: 'James Harden', username: 'jharden13', email: 'jharden13@rockets.com', password: '000000', bio: 'Rockets champions 2020 #NBA' }
       ])
     })
 }
