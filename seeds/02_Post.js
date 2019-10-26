20e13965d88328ea760936018584c122cf1cdfc6
Alvin Ng <ngalvin93@gmail.com>
2019-10-26 00:04:05 -0500

    dashboard render

--- seeds/02_Post.js
+++ seeds/02_Post.js
@@ -4,7 +4,7 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('Post').insert([
-        { userId: 3, imagePath: '/uploads/5eb31158b407bd0e377d.jpeg', caption: 'Digitial crafts is lyfe'},
+        { userId: 3, imagePath: '/uploads/5eb31158b407bd0e377d.jpeg', caption: 'Digitial crafts is lyfe' },
         { userId: 1, imagePath: '/uploads/6cfc035fee0950025d2c.jpeg', caption: 'Turn around dont drown' },
         { userId: 4, imagePath: '/uploads/69aae1bb895aef040e26.jpeg', caption: '🔥🔥🔥🔥🔥🔥' },
         { userId: 4, imagePath: '/uploads/892ba56277cf564f4379.jpeg', caption: 'OMG WTF DIS IS CRAY' },
